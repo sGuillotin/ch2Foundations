3ca9b4d38271eec6c447591c548dbe0ae87c66a3
Sébastien Guillotin <sebgusa@gmail.com>
2025-09-16 09:13:04 -0400

    Get started

--- ch2exercise1/ch2exercise1/Program.cs
+++ ch2exercise1/ch2exercise1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+OperatorExamples();
+
+void OperatorExamples()
+{
+    // This statement declares a variable and sets it to 3
+    int width = 3;
+
+    // The ++ operator increments a variable (add 1 to it)
+    width++;
+
+    // Declare two more int variables to hod numbers and
+    // use the + and * operators to add and and multiply values
+    int height = 2 + 4;
+    int area = width * height;
+    Console.WriteLine(area);
+
+
+    int myfavoritenumber = 7;
+    int MyFavoriteNumber = 10;
+    int my_favorite_number = 14;
+
+}
